c3e7b9586baf65620fb765c95654b20ee1069842
AdamKaderbhai <adamkaderbhai36@gmail.com>
2025-08-27 21:59:06 -0500

    Create ClassNotes8.27.R

---
+++ ClassNotes8.27.R
@@ -0,0 +1,22 @@
+#Class Notes wednesday
+
+library(readxl)
+scooby <- read+excel("scooby.xlsx")
+
+mean(scooby$imdb)
+?mean
+mean(scooby$imdb, na.rm = TRUE)
+
+
+
+# Which shows had the best imdb ratings?
+library(tidyverse)
+
+# Every plot has three pieces of info needed
+# 1.) Dataset
+# 2.) The variables of Interest in the plot & where or how they are displayed
+# 3.) The sort of plot
+
+ggplot(scooby, aes(y = series_name, 
+                   x = imdb)) +
+  geom_boxplot()
